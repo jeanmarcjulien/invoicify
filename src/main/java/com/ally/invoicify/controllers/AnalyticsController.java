30c1da1d49e2a8c427b51e1de33a653e8c0ff35f
mpmiller309 <mpmiller309@gmail.com>
2019-07-25 19:16:38 -0400

    fixed json output format

--- src/main/java/com/ally/invoicify/controllers/AnalyticsController.java
+++ src/main/java/com/ally/invoicify/controllers/AnalyticsController.java
@@ -25,60 +25,97 @@ public class AnalyticsController {
 	CompanyRepository companyRepo;
     
     @GetMapping
-    public InvPerMonth list() {
-        InvPerMonth invXmonth = new InvPerMonth();
+    public List<InvPerMonth> list() {
+        List<InvPerMonth> invXmonthList = new ArrayList<InvPerMonth>();
+        InvPerMonth jan = new InvPerMonth("jan", 0);
+        InvPerMonth feb = new InvPerMonth("feb", 0);
+        InvPerMonth mar = new InvPerMonth("mar", 0);
+        InvPerMonth apr = new InvPerMonth("apr", 0);
+        InvPerMonth may = new InvPerMonth("may", 0);
+        InvPerMonth jun = new InvPerMonth("jun", 0);
+        InvPerMonth jul = new InvPerMonth("jul", 0);
+        InvPerMonth aug = new InvPerMonth("aug", 0);
+        InvPerMonth sep = new InvPerMonth("sep", 0);
+        InvPerMonth oct = new InvPerMonth("oct", 0);
+        InvPerMonth nov = new InvPerMonth("nov", 0);
+        InvPerMonth dec = new InvPerMonth("dec", 0);
 
-        // Loop through invoices, add to month counters
+        // Loop through invoices, set new value for months
         for (Invoice invoice : invoiceRepository.findAll()) {
             switch (invoice.getCreatedOn().getMonth()) {
                 case 0:
-                    invXmonth.setJan(invXmonth.getJan() + 1);
+                    jan.setValue(jan.getValue() + 1);
                     break;
                 case 1:
-                    invXmonth.setFeb(invXmonth.getFeb() + 1);
+                    feb.setValue(feb.getValue() + 1);
                     break;
                 case 2:
-                    invXmonth.setMar(invXmonth.getMar() + 1);
+                    mar.setValue(mar.getValue() + 1);
                     break;
                 case 3:
-                    invXmonth.setApr(invXmonth.getApr() + 1);
+                    apr.setValue(apr.getValue() + 1);
                     break;
                 case 4:
-                    invXmonth.setMay(invXmonth.getMay() + 1);
+                    may.setValue(may.getValue() + 1);
                     break;
                 case 5:
-                    invXmonth.setJun(invXmonth.getJun() + 1);
+                    jun.setValue(jun.getValue() + 1);
                     break;
                 case 6:
-                    invXmonth.setJul(invXmonth.getJul() + 1);
+                    jul.setValue(jul.getValue() + 1);
                     break;
                 case 7:
-                    invXmonth.setAug(invXmonth.getAug() + 1);
+                    aug.setValue(aug.getValue() + 1);
                     break;
                 case 8:
-                    invXmonth.setSep(invXmonth.getSep() + 1);
+                    sep.setValue(sep.getValue() + 1);
                     break;
                 case 9:
-                    invXmonth.setOct(invXmonth.getOct() + 1);
+                    oct.setValue(oct.getValue() + 1);
                     break;
                 case 10:
-                    invXmonth.setNov(invXmonth.getNov() + 1);
+                    nov.setValue(nov.getValue() + 1);
                     break;
                 case 11:
-                    invXmonth.setDec(invXmonth.getDec() + 1);
+                    dec.setValue(dec.getValue() + 1);
                     break;
                 default:
                     break;
             }
         }
 
-        return invXmonth;
+        invXmonthList.add(jan);
+        invXmonthList.add(feb);
+        invXmonthList.add(mar);
+        invXmonthList.add(apr);
+        invXmonthList.add(may);
+        invXmonthList.add(jun);
+        invXmonthList.add(jul);
+        invXmonthList.add(aug);
+        invXmonthList.add(sep);
+        invXmonthList.add(oct);
+        invXmonthList.add(nov);
+        invXmonthList.add(dec);
+
+        return invXmonthList;
     }
 
     @GetMapping("/client/{clientId}")
-    public InvPerMonth listClient(@PathVariable long clientId) {
-        InvPerMonth invXmonth = new InvPerMonth();
+    public List<InvPerMonth> listClient(@PathVariable long clientId) {
+        List<InvPerMonth> invXmonthList = new ArrayList<InvPerMonth>();
         List<Invoice> clientInvoices = new ArrayList<Invoice>();
+        InvPerMonth jan = new InvPerMonth("jan", 0);
+        InvPerMonth feb = new InvPerMonth("feb", 0);
+        InvPerMonth mar = new InvPerMonth("mar", 0);
+        InvPerMonth apr = new InvPerMonth("apr", 0);
+        InvPerMonth may = new InvPerMonth("may", 0);
+        InvPerMonth jun = new InvPerMonth("jun", 0);
+        InvPerMonth jul = new InvPerMonth("jul", 0);
+        InvPerMonth aug = new InvPerMonth("aug", 0);
+        InvPerMonth sep = new InvPerMonth("sep", 0);
+        InvPerMonth oct = new InvPerMonth("oct", 0);
+        InvPerMonth nov = new InvPerMonth("nov", 0);
+        InvPerMonth dec = new InvPerMonth("dec", 0);
 
         // Look at all invoices, if clientId matches add it to clientInvoices
         for (Invoice invoice : invoiceRepository.findAll()) {
@@ -87,57 +124,82 @@ public class AnalyticsController {
             }
         }
 
-        // Loop through invoices, add to month counters
+        // Loop through invoices, set new value for months
         for (Invoice invoice : clientInvoices) {
             switch (invoice.getCreatedOn().getMonth()) {
                 case 0:
-                    invXmonth.setJan(invXmonth.getJan() + 1);
+                    jan.setValue(jan.getValue() + 1);
                     break;
                 case 1:
-                    invXmonth.setFeb(invXmonth.getFeb() + 1);
+                    feb.setValue(feb.getValue() + 1);
                     break;
                 case 2:
-                    invXmonth.setMar(invXmonth.getMar() + 1);
+                    mar.setValue(mar.getValue() + 1);
                     break;
                 case 3:
-                    invXmonth.setApr(invXmonth.getApr() + 1);
+                    apr.setValue(apr.getValue() + 1);
                     break;
                 case 4:
-                    invXmonth.setMay(invXmonth.getMay() + 1);
+                    may.setValue(may.getValue() + 1);
                     break;
                 case 5:
-                    invXmonth.setJun(invXmonth.getJun() + 1);
+                    jun.setValue(jun.getValue() + 1);
                     break;
                 case 6:
-                    invXmonth.setJul(invXmonth.getJul() + 1);
+                    jul.setValue(jul.getValue() + 1);
                     break;
                 case 7:
-                    invXmonth.setAug(invXmonth.getAug() + 1);
+                    aug.setValue(aug.getValue() + 1);
                     break;
                 case 8:
-                    invXmonth.setSep(invXmonth.getSep() + 1);
+                    sep.setValue(sep.getValue() + 1);
                     break;
                 case 9:
-                    invXmonth.setOct(invXmonth.getOct() + 1);
+                    oct.setValue(oct.getValue() + 1);
                     break;
                 case 10:
-                    invXmonth.setNov(invXmonth.getNov() + 1);
+                    nov.setValue(nov.getValue() + 1);
                     break;
                 case 11:
-                    invXmonth.setDec(invXmonth.getDec() + 1);
+                    dec.setValue(dec.getValue() + 1);
                     break;
                 default:
                     break;
             }
         }
 
-        return invXmonth;
+        invXmonthList.add(jan);
+        invXmonthList.add(feb);
+        invXmonthList.add(mar);
+        invXmonthList.add(apr);
+        invXmonthList.add(may);
+        invXmonthList.add(jun);
+        invXmonthList.add(jul);
+        invXmonthList.add(aug);
+        invXmonthList.add(sep);
+        invXmonthList.add(oct);
+        invXmonthList.add(nov);
+        invXmonthList.add(dec);
+
+        return invXmonthList;
     }
 
     @GetMapping("/user/{userId}/graph")
-    public InvPerMonth listGraph(@PathVariable long userId) {
-        InvPerMonth invXmonth = new InvPerMonth();
+    public List<InvPerMonth> listGraph(@PathVariable long userId) {
         List<Invoice> userInvoices = new ArrayList<Invoice>();
+        List<InvPerMonth> invXmonthList = new ArrayList<InvPerMonth>();
+        InvPerMonth jan = new InvPerMonth("jan", 0);
+        InvPerMonth feb = new InvPerMonth("feb", 0);
+        InvPerMonth mar = new InvPerMonth("mar", 0);
+        InvPerMonth apr = new InvPerMonth("apr", 0);
+        InvPerMonth may = new InvPerMonth("may", 0);
+        InvPerMonth jun = new InvPerMonth("jun", 0);
+        InvPerMonth jul = new InvPerMonth("jul", 0);
+        InvPerMonth aug = new InvPerMonth("aug", 0);
+        InvPerMonth sep = new InvPerMonth("sep", 0);
+        InvPerMonth oct = new InvPerMonth("oct", 0);
+        InvPerMonth nov = new InvPerMonth("nov", 0);
+        InvPerMonth dec = new InvPerMonth("dec", 0);
 
         // Look at all invoices, if userId matches add it to userInvoices
         for (Invoice invoice : invoiceRepository.findAll()) {
@@ -146,50 +208,62 @@ public class AnalyticsController {
             }
         }
 
-        // Loop through invoices, add to month counters
+        // Loop through invoices, set new value for months
         for (Invoice invoice : userInvoices) {
             switch (invoice.getCreatedOn().getMonth()) {
                 case 0:
-                    invXmonth.setJan(invXmonth.getJan() + 1);
+                    jan.setValue(jan.getValue() + 1);
                     break;
                 case 1:
-                    invXmonth.setFeb(invXmonth.getFeb() + 1);
+                    feb.setValue(feb.getValue() + 1);
                     break;
                 case 2:
-                    invXmonth.setMar(invXmonth.getMar() + 1);
+                    mar.setValue(mar.getValue() + 1);
                     break;
                 case 3:
-                    invXmonth.setApr(invXmonth.getApr() + 1);
+                    apr.setValue(apr.getValue() + 1);
                     break;
                 case 4:
-                    invXmonth.setMay(invXmonth.getMay() + 1);
+                    may.setValue(may.getValue() + 1);
                     break;
                 case 5:
-                    invXmonth.setJun(invXmonth.getJun() + 1);
+                    jun.setValue(jun.getValue() + 1);
                     break;
                 case 6:
-                    invXmonth.setJul(invXmonth.getJul() + 1);
+                    jul.setValue(jul.getValue() + 1);
                     break;
                 case 7:
-                    invXmonth.setAug(invXmonth.getAug() + 1);
+                    aug.setValue(aug.getValue() + 1);
                     break;
                 case 8:
-                    invXmonth.setSep(invXmonth.getSep() + 1);
+                    sep.setValue(sep.getValue() + 1);
                     break;
                 case 9:
-                    invXmonth.setOct(invXmonth.getOct() + 1);
+                    oct.setValue(oct.getValue() + 1);
                     break;
                 case 10:
-                    invXmonth.setNov(invXmonth.getNov() + 1);
+                    nov.setValue(nov.getValue() + 1);
                     break;
                 case 11:
-                    invXmonth.setDec(invXmonth.getDec() + 1);
+                    dec.setValue(dec.getValue() + 1);
                     break;
                 default:
                     break;
             }
         }
+        invXmonthList.add(jan);
+        invXmonthList.add(feb);
+        invXmonthList.add(mar);
+        invXmonthList.add(apr);
+        invXmonthList.add(may);
+        invXmonthList.add(jun);
+        invXmonthList.add(jul);
+        invXmonthList.add(aug);
+        invXmonthList.add(sep);
+        invXmonthList.add(oct);
+        invXmonthList.add(nov);
+        invXmonthList.add(dec);
 
-        return invXmonth;
+        return invXmonthList;
     }
 }
