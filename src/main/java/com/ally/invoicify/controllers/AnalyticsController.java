fd969c5afaa2b39a6f02a59e8e5e3d8cac67179f
mpmiller309 <mpmiller309@gmail.com>
2019-07-25 13:29:14 -0400

    code cleanup

--- src/main/java/com/ally/invoicify/controllers/AnalyticsController.java
+++ src/main/java/com/ally/invoicify/controllers/AnalyticsController.java
@@ -1,16 +1,7 @@
 package com.ally.invoicify.controllers;
 
-import java.util.List;
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
