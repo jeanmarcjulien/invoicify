ab1737bf9274374e8df35d833bde9b8577fcc167
chickok <crhicko@g.clemson.edu>
2019-07-26 14:35:04 -0400

    invoice team select

--- src/main/java/com/ally/invoicify/controllers/BillingRecordController.java
+++ src/main/java/com/ally/invoicify/controllers/BillingRecordController.java
@@ -3,6 +3,7 @@ package com.ally.invoicify.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +14,24 @@ import com.ally.invoicify.repositories.CompanyRepository;
 @RestController
 @RequestMapping("/api/billing-record")
 public class BillingRecordController {
-	
+
 	private BillingRecordRepository recordRepository;
 
 	public BillingRecordController(BillingRecordRepository recordRepository, CompanyRepository companyRepository) {
 		this.recordRepository = recordRepository;
 	}
 
+	@GetMapping("/{id}")
+	public List<BillingRecord> listById(@PathVariable long id) {
+		// return "hi";
+		return recordRepository.findByClientId(id);
+	}
+
 	@GetMapping
 	public List<BillingRecord> list() {
 		return recordRepository.findAll();
 	}
-	
+
+
+
 }
