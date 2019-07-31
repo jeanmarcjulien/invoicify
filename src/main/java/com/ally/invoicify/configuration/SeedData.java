f48d9a318c9ef4c581843341db9bda027cbb5f05
JoelSaju <52932597+JoelSaju@users.noreply.github.com>
2019-07-31 14:13:26 -0400

    Populating Client data

--- src/main/java/com/ally/invoicify/configuration/SeedData.java
+++ src/main/java/com/ally/invoicify/configuration/SeedData.java
@@ -15,23 +15,50 @@ import com.ally.invoicify.repositories.UserRepository;
 public class SeedData {
 
 	public SeedData(BillingRecordRepository recordRepository, CompanyRepository companyRepository,
-			UserRepository userRepository, PasswordEncoder encoder) {
+			UserRepository userRepository, PasswordEncoder./ encoder) {
 		User admin = userRepository.save(new User("admin", encoder.encode("admin")));
 
-		Company ajax = companyRepository.save(new Company("AJAX Ltd."));
-		Company lomax = companyRepository.save(new Company("Lomax Brothers, LLC"));
+		Company vapianos = companyRepository.save(new Company("Vapianos Ltd."));
+		Company merts = companyRepository.save(new Company("Merts, LLC"));
+		Company redeyediner = companyRepository.save(new Company("Red Eye Diner, LLC"));
+		Company aria = companyRepository.save(new Company("Aria, LLC"));
+		Company pinkysgrill = companyRepository.save(new Company("Pinky's Grill, LLC"));
+		Company salspizza = companyRepository.save(new Company("Sal's Pizza, LLC"));
+		Company selenium = companyRepository.save(new Company("Selenium, LLC"));
+		Company zentaco = companyRepository.save(new Company("Zen Taco, LLC"));
+		Company bbb = companyRepository.save(new Company("Big Baller Brand, LLC"));
 
-		recordRepository.save(new FlatFeeBillingRecord(300, "Faxes", ajax, admin));
-		recordRepository.save(new FlatFeeBillingRecord(1.75, "Socks", ajax, admin));
-		recordRepository.save(new FlatFeeBillingRecord(500, "Paper", lomax, admin));
-		recordRepository.save(new FlatFeeBillingRecord(72.33, "Stockings", lomax, admin));
-		recordRepository.save(new FlatFeeBillingRecord(142.99, "Paint", lomax, admin));
+		recordRepository.save(new FlatFeeBillingRecord(300, "Faxes", vapianos, admin));
+		recordRepository.save(new FlatFeeBillingRecord(1.75, "Socks", redeyediner, admin));
+		recordRepository.save(new FlatFeeBillingRecord(500, "Paper", merts, admin));
+		recordRepository.save(new FlatFeeBillingRecord(72.33, "Stockings", aria, admin));
+		recordRepository.save(new FlatFeeBillingRecord(142.99, "Paint", redeyediner, admin));
+		recordRepository.save(new FlatFeeBillingRecord(300, "Faxes", vapianos, admin));
+		recordRepository.save(new FlatFeeBillingRecord(1.75, "Socks", aria, admin));
+		recordRepository.save(new FlatFeeBillingRecord(500, "Paper", selenium, admin));
+		recordRepository.save(new FlatFeeBillingRecord(72.33, "Stockings", merts, admin));
+		recordRepository.save(new FlatFeeBillingRecord(142.99, "Paint", zentaco, admin));
+		recordRepository.save(new FlatFeeBillingRecord(300, "Faxes", salspizza, admin));
+		recordRepository.save(new FlatFeeBillingRecord(1.75, "Socks", aria, admin));
+		recordRepository.save(new FlatFeeBillingRecord(500, "Paper", redeyediner, admin));
+		recordRepository.save(new FlatFeeBillingRecord(72.33, "Stockings", bbb, admin));
+		recordRepository.save(new FlatFeeBillingRecord(142.99, "Paint", selenium, admin));
 
-		recordRepository.save(new RateBasedBillingRecord(500, 3.5, "Legal services", ajax, admin));
-		recordRepository.save(new RateBasedBillingRecord(150, 2.5, "Painting", ajax, admin));
-		recordRepository.save(new RateBasedBillingRecord(100, 4.25, "House cleaning", ajax, admin));
-		recordRepository.save(new RateBasedBillingRecord(700, 8, "Palm reading", lomax, admin));
-		recordRepository.save(new RateBasedBillingRecord(1.57, 25, "Show shining", lomax, admin));
+		recordRepository.save(new RateBasedBillingRecord(500, 3.5, "Legal services", vapianos, admin));
+		recordRepository.save(new RateBasedBillingRecord(150, 2.5, "Painting", redeyediner, admin));
+		recordRepository.save(new RateBasedBillingRecord(100, 4.25, "House cleaning", zentaco, admin));
+		recordRepository.save(new RateBasedBillingRecord(700, 8, "Lighting", merts, admin));
+		recordRepository.save(new RateBasedBillingRecord(1.57, 25, "Show shining", redeyediner, admin));
+		recordRepository.save(new RateBasedBillingRecord(500, 3.5, "Legal services", pinkysgrill, admin));
+		recordRepository.save(new RateBasedBillingRecord(150, 2.5, "Painting", aria, admin));
+		recordRepository.save(new RateBasedBillingRecord(100, 4.25, "House cleaning", bbb, admin));
+		recordRepository.save(new RateBasedBillingRecord(700, 8, "Lighting", selenium, admin));
+		recordRepository.save(new RateBasedBillingRecord(1.57, 25, "Show shining", redeyediner, admin));
+		recordRepository.save(new RateBasedBillingRecord(500, 3.5, "Legal services", salspizza, admin));
+		recordRepository.save(new RateBasedBillingRecord(150, 2.5, "Painting", zentaco, admin));
+		recordRepository.save(new RateBasedBillingRecord(100, 4.25, "House cleaning", salspizza, admin));
+		recordRepository.save(new RateBasedBillingRecord(700, 8, "Lighting", pinkysgrill, admin));
+		recordRepository.save(new RateBasedBillingRecord(1.57, 25, "Show shining", redeyediner, admin));
 	}
 
 }
