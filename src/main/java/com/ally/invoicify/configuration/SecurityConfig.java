d4b57bfbb400b87e2f5a7fc5e0babc565ae15686
jeanmarcjulien <julienjeanmarc@gmail.com>
2021-07-22 05:58:05 -0500

    adds cors

--- src/main/java/com/ally/invoicify/configuration/SecurityConfig.java
+++ src/main/java/com/ally/invoicify/configuration/SecurityConfig.java
@@ -1,8 +1,8 @@
-package com.ally.invoicify.configuration;
+package com.galvanize.invoicify.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,32 +10,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.ally.invoicify.services.AppUserDetailsService;
+import com.galvanize.invoicify.services.AppUserDetailsService;
+import org.springframework.web.cors.CorsConfiguration;
 
-@Configuration
+import java.util.Arrays;
+
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private AppUserDetailsService userDetailsService;
-
-    public SecurityConfig(AppUserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    //allow POST routes to /api/user and /api/session endpoints
-    //require authorization for everything else
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http    
-            .authorizeRequests()
-                .antMatchers(HttpMethod.PUT, "/api/session").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/user").permitAll()    
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
-                .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**").permitAll()
-                // .anyRequest().authenticated()
-                .and()
-                .csrf().disable();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -45,6 +27,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public UserDetailsService userDetailsService() {
         return userDetailsService;
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    private AppUserDetailsService userDetailsService;
+
+    public SecurityConfig(AppUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+
+        // You can customize the following part based on your project, it's only a sample
+        http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/session").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/user").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+//                .anyRequest().authenticated()
+                .and().csrf().disable().cors().configurationSource(request -> corsConfiguration);
     }
 
 }
